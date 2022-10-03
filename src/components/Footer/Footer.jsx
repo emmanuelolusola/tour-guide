73b56798a4b4492a5ceac7d6065fc46d9217a9ee
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-10-03 18:17:28 +0100

    all components included - incomplete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,135 @@
 import React from "react";
 import "./footer.css";
+import video2 from "../../assets/video2.mp4";
+
+import { FiSend, FiChevronRight } from "react-icons/fi";
+import { SiYourtraveldottv } from "react-icons/si";
+import { FaTripadvisor } from "react-icons/fa";
+import {
+  AiOutlineTwitter,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter Email Address" />
+            <button className="btn flex" type="submit">
+              SEND
+              <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <SiYourtraveldottv className="icon" /> Tour Guide
+              </a>
+            </div>
+            <div className="footerParagraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus beatae minima in quos possimus ipsam, facere enim
+              dolorum at. Quaerat sit repudiandae dolore veritatis nisi
+              assumenda suscipit molestiae tempora nihil?
+            </div>
+            <div className="footerSocials">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS </span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+          <div className="footerDiv flex">
+            <small>SAMPLE TOUR GUIDE WEBSITE THEME</small>
+            <small>COPYRIGHT RESERVED - EMMANUEL OLUSOLA 2022</small>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
